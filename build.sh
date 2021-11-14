11b29654849d59a88b6d7091ca9d44eebdd86a2a
dgey <david.gey@thank-the-maker.org>
2021-11-14 11:45:23 +0100

    initial version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # rm -fr build dist
-VERSION=5.01.
+VERSION=5.0.1
 NAME="rESCue-flasher"
 DIST_NAME="rESCue-flasher"
 
@@ -12,3 +12,8 @@ pyinstaller --log-level=DEBUG \
 # https://github.com/sindresorhus/create-dmg
 create-dmg "dist/$NAME.dmg" "dist/$NAME.app"
 #mv "$NAME $VERSION.dmg" "dist/$DIST_NAME.dmg"
+
+pyinstaller --log-level=DEBUG \
+            --noconfirm \
+            --windowed \
+	    build-on-win.spec
